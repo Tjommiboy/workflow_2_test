daa65b3196fff98410c25ac6bddba15c8036cb5a
Tjommiboy <119463874+Tjommiboy@users.noreply.github.com>
2025-06-23 16:46:46 +0700

    ran npm build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Dynamically set base depending on environment
 export default defineConfig({
   plugins: [react()],
   base: "/workflow_2_test/",
